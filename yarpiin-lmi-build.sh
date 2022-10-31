520dbc0936db7ebdf7207a390a9c568fcd7b1f54
ZenkaBestia <z.mail.6600@gmail.com>
2022-10-31 02:49:24 +0000

    Update build script for lmi

--- yarpiin-lmi-build.sh
+++ yarpiin-lmi-build.sh
@@ -10,7 +10,7 @@ clear
 
 # Resources
 THREAD="-j$(grep -c ^processor /proc/cpuinfo)"
-KERNEL="Image.gz"
+KERNEL="Image"
 DTBOIMG="dtbo.img"
 
 # Defconfigs
@@ -69,12 +69,16 @@ function make_lmi_kernel {
         export TARGET_PRODUCT=lmi
         make O=out ARCH=arm64 $LMIDEFCONFIG
 
-        PATH="$CLANG_DIR:$GCC_DIR:${PATH}" \
-        make -j$(nproc --all) O=out \
-                      ARCH=arm64 \
-                      CC=clang \
-                      CLANG_TRIPLE=aarch64-linux-gnu- \
-                      CROSS_COMPILE=aarch64-linux-android-
+	    make -j$(nproc) O=out ARCH=arm64 ${LMIDEFCONFIG}
+	    make -j$(nproc) ARCH=arm64 O=out \
+		    CC=${CLANG_DIR}/clang \
+		    AR=${CLANG_DIR}/llvm-ar \
+		    NM=${CLANG_DIR}/llvm-nm \
+		    OBJCOPY=${CLANG_DIR}/llvm-objcopy \
+		    OBJDUMP=${CLANG_DIR}/llvm-objdump \
+		    STRIP=${CLANG_DIR}/llvm-strip \
+		    LD=${CLANG_DIR}/ld.lld \
+		    CROSS_COMPILE=aarch64-linux-gnu-
 
 		cp -vr $ZIMAGE_DIR/$KERNEL $KERNELFLASHER_DIR
 		cp -vr $ZIMAGE_DIR/$DTBOIMG $KERNELFLASHER_DIR
